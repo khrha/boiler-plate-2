b796b52e8f3796e62938a1742c159d66308f1edb
khrha <mrt.xener@gmail.com>
2022-05-03 22:18:48 +0900

    정보 보호

--- index.js
+++ index.js
@@ -2,16 +2,36 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const bodyParser = require('body-parser');
+const config = require('./config/key');
+const { User } = require("./models/User");
+
+app.use(bodyParser.urlencoded({extended: true}));
+
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://manager:manager@cluster0.bwm2a.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
-  useNewUrlParser: true, useUnifiedTopology: true
+mongoose.connect(config.mongoURI, {
+  useNewUrlParser: true, useUnifiedTopology: true //not supported, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err))
 
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World! 안녕하세요')
 })
+
+app.post('/register', (req, res) => {
+  // 회원가입할 때 필요한 정보를 client에서 가져오면
+  // 데이터베이스에 넣는다.
+  const user = new User(req.body);
+
+  user.save((err, userInfo) => {
+    if(err) return res.json({success: false, err})
+    return res.status(200).json({
+      success: true,
+    })
+  })
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
